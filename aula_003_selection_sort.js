3db2b6ba003dbab1b8d5624de061518eab3af8e8
wagner <wagner.santos@accurate.com.br>
2023-02-27 15:52:27 -0300

    Exemplo de Selection Sort com forEach

--- aula_003_selection_sort.js
+++ aula_003_selection_sort.js
@@ -1,19 +1,34 @@
 const livros = require("./aula_002_lista_livros");
 const menorValor = require("./aula_003_function_menor_valor");
 
-for (let atual = 0; atual < livros.length-1; atual++) {
-  let menor = menorValor(livros, atual);
+livros.forEach((_, indice) => {
+  let menor = menorValor(livros, indice);
 
-  console.log("Posicao atual: ", atual);
+  console.log(`Posição atual: ${indice}`);
 
-  let livroAtual = livros[atual];
-  console.log("livro atual: ", livroAtual);
+  let livroAtual = livros[indice];
+  console.log("Livro atual: ", livroAtual);
 
   let livroMenorPreco = livros[menor];
-  console.log("livro menor preco: ", livroMenorPreco);
+  console.log("Livro de menor preço: ", livroMenorPreco);
 
-  livros[atual] = livroMenorPreco;
+  livros[indice] = livroMenorPreco;
   livros[menor] = livroAtual;
-}
+});
+
+// for (let atual = 0; atual < livros.length-1; atual++) {
+//   let menor = menorValor(livros, atual);
+
+//   console.log("Posicao atual: ", atual);
+
+//   let livroAtual = livros[atual];
+//   console.log("livro atual: ", livroAtual);
+
+//   let livroMenorPreco = livros[menor];
+//   console.log("livro menor preco: ", livroMenorPreco);
+
+//   livros[atual] = livroMenorPreco;
+//   livros[menor] = livroAtual;
+// }
 
 console.log(livros);
