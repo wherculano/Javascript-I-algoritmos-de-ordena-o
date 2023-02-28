b9d20d9f1777797d7d1a5304bf2f64e72b28d5ad
wagner <wagner.santos@accurate.com.br>
2023-02-28 08:05:47 -0300

    Exemplo de Insertion Sorte reutilizando codigo

--- aula_004_insertion_sort.js
+++ aula_004_insertion_sort.js
@@ -1,16 +1,11 @@
 const livros = require("./aula_002_lista_livros");
-const menorValor = require("./aula_003_function_menor_valor");
+const troca = require("./aula_004_function_troca")
 
 function insertionSort(lista){
     for(let atual=0; atual < lista.length; atual++){
         let analise = atual
         while(analise > 0 && lista[analise].preco < lista[analise - 1].preco){
-            itemAnalise = lista[analise]
-            itemAnterior = lista[analise - 1]
-
-            lista[analise] = itemAnterior
-            lista[analise - 1] = itemAnalise
-
+            troca(lista, analise)
             analise--
         }
     }
